7941d0decc0763b789642a990713a18869e546b6
Thanima Manoj <thanimamanoj44@gmail.com>
2021-12-05 18:19:18 +0530

    Added ability to drag and drop categories

--- app/javascript/src/components/Settings/ManageCategories/CategoryList.jsx
+++ app/javascript/src/components/Settings/ManageCategories/CategoryList.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
-import { Plus, UpArrow, Edit, Delete, Check } from "@bigbinary/neeto-icons";
+import { Plus, Edit, Delete, Check } from "@bigbinary/neeto-icons";
 import { Typography, Input, Button } from "@bigbinary/neetoui/v2";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const CategoryList = ({
   categories,
@@ -38,39 +39,63 @@ const CategoryList = ({
             <Check onClick={() => handleAdd(name)} />
           </div>
         )}
-        {categories.map(({ id, name }) => (
-          <div key={id}>
-            {editCategory === id ? (
-              <div className="flex">
-                <Input
-                  value={editCategoryName}
-                  onChange={e => setEditCategoryName(e.target.value)}
-                />
-                <Check onClick={() => handleEdit(id, editCategoryName)} />
-              </div>
-            ) : (
-              <div className="flex">
-                <UpArrow size={16} className="flex items-center h-12" />
-                <Typography style="h5" className="flex items-center h-12">
-                  {name}
-                </Typography>
-                <Delete
-                  size={18}
-                  onClick={() => handleDelete(id)}
-                  className="flex items-center h-12 ml-auto cursor-pointer mr-2"
-                />
-                <Edit
-                  size={18}
-                  onClick={() => {
-                    setEditCategoryName(name);
-                    setEditCategory(id);
-                  }}
-                  className="flex items-center h-12 cursor-pointer"
-                />
-              </div>
+        <DragDropContext>
+          <Droppable droppableId="categories">
+            {provided => (
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+                {categories.map(({ id, name }, index) => (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {provided => (
+                      <li
+                        {...provided.dragHandleProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                      >
+                        {editCategory === id ? (
+                          <div className="flex">
+                            <Input
+                              value={editCategoryName}
+                              onChange={e =>
+                                setEditCategoryName(e.target.value)
+                              }
+                            />
+                            <Check
+                              onClick={() => handleEdit(id, editCategoryName)}
+                            />
+                          </div>
+                        ) : (
+                          <div className="flex">
+                            <i className="ri-drag-move-2-fill flex items-center h-12 mr-2" />
+                            <Typography
+                              style="h5"
+                              className="flex items-center h-12"
+                            >
+                              {name}
+                            </Typography>
+                            <Delete
+                              size={18}
+                              onClick={() => handleDelete(id)}
+                              className="flex items-center h-12 ml-auto cursor-pointer mr-2"
+                            />
+                            <Edit
+                              size={18}
+                              onClick={() => {
+                                setEditCategoryName(name);
+                                setEditCategory(id);
+                              }}
+                              className="flex items-center h-12 cursor-pointer"
+                            />
+                          </div>
+                        )}
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
             )}
-          </div>
-        ))}
+          </Droppable>
+        </DragDropContext>
       </div>
     </div>
   );
